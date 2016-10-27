6ba0686941f7f9ed0cf74ef2ee54373d1130b44c
RoyalflushLiu <q5664209@gmail.com>
2016-10-27 15:44:12 -0500

    Add drive train

--- vexuser.c
+++ vexuser.c
@@ -48,31 +48,31 @@
 
 // Digi IO configuration
 static  vexDigiCfg  dConfig[kVexDigital_Num] = {
-        { kVexDigital_1,    kVexSensorDigitalOutput, kVexConfigOutput,      0 },
-        { kVexDigital_2,    kVexSensorDigitalOutput, kVexConfigOutput,      0 },
-        { kVexDigital_3,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_4,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_5,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_6,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_7,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_8,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_9,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_10,   kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_11,   kVexSensorDigitalInput,  kVexConfigInput,       0 },
-        { kVexDigital_12,   kVexSensorDigitalInput,  kVexConfigInput,       0 }
+		{ kVexDigital_1,    kVexSensorDigitalOutput, kVexConfigOutput,      0 },
+		{ kVexDigital_2,    kVexSensorDigitalOutput, kVexConfigOutput,      0 },
+		{ kVexDigital_3,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_4,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_5,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_6,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_7,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_8,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_9,    kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_10,   kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_11,   kVexSensorDigitalInput,  kVexConfigInput,       0 },
+		{ kVexDigital_12,   kVexSensorDigitalInput,  kVexConfigInput,       0 }
 };
 
 static  vexMotorCfg mConfig[kVexMotorNum] = {
-        { kVexMotor_1,      kVexMotor393T,           kVexMotorNormal,       kVexSensorIME,         kImeChannel_1 },
-        { kVexMotor_2,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_3,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_4,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_5,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_6,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_7,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_8,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_9,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
-        { kVexMotor_10,     kVexMotor393T,           kVexMotorNormal,       kVexSensorIME,         kImeChannel_2 }
+		{ kVexMotor_1,      kVexMotor393T,           kVexMotorNormal,       kVexSensorIME,         kImeChannel_1 },
+		{ kVexMotor_2,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_3,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_4,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_5,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_6,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_7,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_8,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_9,      kVexMotorUndefined,      kVexMotorNormal,       kVexSensorNone,        0 },
+		{ kVexMotor_10,     kVexMotor393T,           kVexMotorNormal,       kVexSensorIME,         kImeChannel_2 }
 };
 
 
@@ -112,62 +112,69 @@ vexUserInit()
 msg_t
 vexAutonomous( void *arg )
 {
-    (void)arg;
+	(void)arg;
 
-    // Must call this
-    vexTaskRegister("auton");
+	// Must call this
+	vexTaskRegister("auton");
 
-    while(1)
-        {
-        // Don't hog cpu
-        vexSleep( 25 );
-        }
+	while(1)
+	{
+		// Don't hog cpu
+		vexSleep( 25 );
+	}
 
-    return (msg_t)0;
+	return (msg_t)0;
 }
 
-#define MotorDriveL     kVexMotor_1
-#define MotorDriveR     kVexMotor_10
 
+#define MOTOR_DRIVE_LEFT_FRONT kVexMotor_3
+#define MOTOR_DRIVE_LEFT_BEHIND kVexMotor_2
+
+#define MOTOR_DRIVE_RIGHT_FRONT kVexMotor_8
+#define MOTOR_DRIVE_RIGHT_BEHIND kVexMotor_9
 /*-----------------------------------------------------------------------------*/
 /** @brief      Driver control                                                 */
 /*-----------------------------------------------------------------------------*/
 /** @details
  *  This thread is started when the driver control period is started
  */
+
+void motorDriveControlLeft(int16_t speed);
+
+void motorDriveControlRight(int16_t speed);
+
 msg_t
 vexOperator( void *arg )
 {
-	int16_t		blink = 0;
-
 	(void)arg;
 
 	// Must call this
 	vexTaskRegister("operator");
 
 	// Run until asked to terminate
-	while(!chThdShouldTerminate())
-		{
-		// flash led/digi out
-		vexDigitalPinSet( kVexDigital_1, (blink++ >> 3) & 1);
+	while(!chThdShouldTerminate()) {
+		int16_t yAxis = vexControllerGet(Ch3);
+		int16_t xAxis = vexControllerGet(Ch1);
 
-		// status on LCD of encoder and sonar
-		vexLcdPrintf( VEX_LCD_DISPLAY_2, VEX_LCD_LINE_1, "%4.2fV   %8.1f", vexSpiGetMainBattery() / 1000.0, chTimeNow() / 1000.0 );
-		vexLcdPrintf( VEX_LCD_DISPLAY_2, VEX_LCD_LINE_2, "L %3d R %3d", vexMotorGet( MotorDriveL ), vexMotorGet( MotorDriveR ) );
-
-		// Tank drive
-		// left drive
-		vexMotorSet( MotorDriveL, vexControllerGet( Ch3 ) );
-
-		// right drive
-		vexMotorSet( MotorDriveR, vexControllerGet( Ch2 ) );
-
+		motorDriveControlRight(yAxis + xAxis);
+		motorDriveControlLeft(yAxis - xAxis);
 		// Don't hog cpu
 		vexSleep( 25 );
-		}
+	}
 
 	return (msg_t)0;
 }
 
+//set the speed of the left motors
+void motorDriveControlLeft(int16_t speed) {
+	vexMotorSet( MOTOR_DRIVE_LEFT_FRONT, speed);
+	vexMotorSet( MOTOR_DRIVE_LEFT_BEHIND, speed);
+}
+
+//set the speed of the right motors
+void motorDriveControlRight(int16_t speed) {
+	vexMotorSet( MOTOR_DRIVE_RIGHT_FRONT, speed);
+	vexMotorSet( MOTOR_DRIVE_RIGHT_BEHIND, speed);
+}
 
 
