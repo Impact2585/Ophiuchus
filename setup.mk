a6c65e7ab1b56384a780a6f939050257cc102067
J0hnCena <joshua99wong@gmail.com>
2016-10-28 17:40:26 -0500

    Add Lift task. Relocate system files to src folder. Add swerve motor and invert front drivetrain motors

--- setup.mk
+++ setup.mk
@@ -8,7 +8,7 @@ CONVEX     = ../convex/cortex
 #CONVEX_OPT  = yes
 
 # User C code files
-VEXUSERSRC = vexuser.c
+VEXUSERSRC = vexuser.c $(wildcard src/*.c)
 
 # Uncomment and add/modify user include files
 #VEXUSERINC = myfile.h
